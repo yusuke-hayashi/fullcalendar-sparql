3993ac223aa726b0ad491a1dffad3bdb30f3581a
Yusuke Hayashi <hayashi@lel.hiroshima-u.ac.jp>
2022-10-18 07:48:27 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-﻿var endpoint = "http://data.yafjp.org/sparql";
+﻿var endpoint = "https://data.yafjp.org/sparql";
 var query = (function () {/*
 PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
